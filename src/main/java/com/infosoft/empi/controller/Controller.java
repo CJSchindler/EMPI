4a1e13b8346d72bdf1dd5ade710c4ea2d2953f1d
RundeCrew <kris.runde@gmail.com>
2018-09-29 12:43:18 -0400

    added addtional controller

--- src/main/java/com/infosoft/empi/controller/Controller.java
+++ src/main/java/com/infosoft/empi/controller/Controller.java
@@ -6,10 +6,16 @@ import org.springframework.web.servlet.ModelAndView;
 public class Controller {
 
 	// shows login page
-		@RequestMapping("/")
-		public ModelAndView showIndex() {
-			ModelAndView mav = new ModelAndView("index");
-			return mav;
-		}
-	
+	@RequestMapping("/")
+	public ModelAndView showIndex() {
+		ModelAndView mav = new ModelAndView("index");
+		return mav;
+	}
+	// new jsp for index
+	@RequestMapping("/home")
+	public ModelAndView showHomePage() {
+		ModelAndView mav = new ModelAndView("/home");
+		return mav;
+	}
+
 }
