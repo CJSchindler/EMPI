c6c75c15771a75556c84a4c0cdc3b30cc66e04ec
Lauren Shindo <lauren.shindo@gmail.com>
2018-09-29 12:39:16 -0400

    updated slider content and logo

--- src/main/java/com/infosoft/empi/controller/Controller.java
+++ src/main/java/com/infosoft/empi/controller/Controller.java
@@ -1,5 +1,15 @@
 package com.infosoft.empi.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 public class Controller {
 
+	// shows login page
+		@RequestMapping("/")
+		public ModelAndView showIndex() {
+			ModelAndView mav = new ModelAndView("index");
+			return mav;
+		}
+	
 }
